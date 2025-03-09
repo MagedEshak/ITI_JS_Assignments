480fb56d8e63174a46188646339a848143ce3a31
Maged Eshak <magedeshak36@gmail.com>
2025-03-09 22:30:28 +0200

    bouns 1 first case

---
+++ Day1/Bonus1/bonus1.js
@@ -0,0 +1,10 @@
+console.log("Day 1 Bonus");
+console.log("-".repeat(20));
+
+function welcome() {
+    for (let i = 1; i <= 6; i++) {
+        document.write(`<h${i}>welcome to my page</h${i}>`);
+    }
+}
+welcome();
+
